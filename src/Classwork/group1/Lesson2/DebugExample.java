96ce9c6caa5b58d14e354658e420fe7e4debe990
HP <nare.vos90@mail.ru>
2022-05-11 19:15:58 +0300

    first commit

---
+++ src/Classwork/group1/Lesson2/DebugExample.java
@@ -0,0 +1,22 @@
+package Classwork.group1.Lesson2;
+
+public class DebugExample {
+
+    /*
+     *
+     * *
+     * * *
+     * * * *
+     * * * * *
+
+     */
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) ;
+        for (int j = 0; j < j; j++) ;
+        System.out.println("*   ");
+        System.out.print("   * ");
+    }
+
+
+}
+
