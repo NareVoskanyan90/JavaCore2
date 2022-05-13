c6e686e867821d9abffa098204b313f3c0344939
HP <nare.vos90@mail.ru>
2022-05-13 15:50:14 +0300

    charDemo2

--- src/Homevork/FigureExample.java
+++ src/Homevork/FigureExample.java
@@ -26,10 +26,10 @@ public static void main(String[] args) {
     }
     for (int i = 0; i < 4; i++) {
         for (int j = 4; j >i; j--) {
-            System.out.print("* ");
+            System.out.print("*");
 
         }
-        for (int j = 1; j >=i; j++) {
+        for ( int j = 1; j <=i; j++) {
             System.out.print(" ");
         }
         System.out.println("");
