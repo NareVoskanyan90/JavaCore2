dc196d922e3bcd345dabf291d56157eabf0540f7
HP <nare.vos90@mail.ru>
2022-05-14 16:42:54 +0300

    charDemo2

--- src/Lesson6/FigurExample.java
+++ src/Lesson6/FigurExample.java
@@ -9,18 +9,17 @@ public class FigurExample {
 
      */
     public static void main(String[] args) {
-        for (int i = 0; i < 4; i++) {
-            for (int j = 4; j >i; j--) {
-                System.out.print(" ");
+        for (int i = 0; i < 5; i++) {
+            for (int j = 4; j <i; j++) {
+                System.out.print("  ");
             }
-            for (int j = 0; j <=i; j++) {
-                System.out.print("*");
-
+            for (int j=5;j>i;j--);
+            System.out.print("* ");
             }
-            System.out.println();
+        System.out.println();
         }
     }
-}
+
 
 
 
