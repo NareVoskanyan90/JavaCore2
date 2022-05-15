0e5bce8ee4fd412f4f0675599d9d5fdc6e302338
HP <nare.vos90@mail.ru>
2022-05-15 19:55:15 +0300

    charDemo2

---
+++ src/Homevork/Arrayutil.java
@@ -0,0 +1,18 @@
+package Homevork;
+
+public class Arrayutil {
+
+    public static void main(String[] args) {
+        int[]array={2,5,8,10,32,20,10,88,11,30};
+for (int i=0;i< array.length;i++) {
+
+    System.out.print(array[i]+" ");
+}
+
+
+     }
+
+     }
+
+
+
