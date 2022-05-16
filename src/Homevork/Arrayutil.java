fd731c6dfe188615a834d4f445f9d64fe13a8191
HP <nare.vos90@mail.ru>
2022-05-16 12:28:07 +0300

    charDemo2

--- src/Homevork/Arrayutil.java
+++ src/Homevork/Arrayutil.java
@@ -3,16 +3,29 @@ package Homevork;
 public class Arrayutil {
 
     public static void main(String[] args) {
-        int[]array={2,5,8,10,32,20,10,88,11,30};
-for (int i=0;i< array.length;i++) {
+        int[] array = {2, 5, 8, 10, 32, 20, 10, 88, 11, 30};
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+        int x = array[0];
+        for (int i = 0; i < array.length; i++) {
 
-    System.out.print(array[i]+" ");
+            x = array[0];
+            if (array[i] > x) {
+                x = array[i];
+                System.out.println(x);
+
+            }
+        }
+    }
 }
 
 
-     }
 
-     }
+
+
+
+
 
 
 
