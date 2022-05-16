83fa09b04fc077d6b5b438c7e7a4cf1a8b9dfaa7
HP <nare.vos90@mail.ru>
2022-05-16 13:23:15 +0300

    charDemo2

--- src/Homevork/Arrayutil.java
+++ src/Homevork/Arrayutil.java
@@ -5,20 +5,30 @@ public class Arrayutil {
     public static void main(String[] args) {
         int[] array = {2, 5, 8, 10, 32, 20, 10, 88, 11, 30};
         for (int i = 0; i < array.length; i++) {
-            System.out.println(array[i]);
+            System.out.print(array[i] + " ");
         }
-        int x = array[0];
-        for (int i = 0; i < array.length; i++) {
+        int max = array[0];
+        for (int i = 1; i < array.length; i++) {
 
-            x = array[0];
-            if (array[i] > x) {
-                x = array[i];
-                System.out.println(x);
+            max = array[0];
+            if (array[i] > max) {
+                max = array[i];
+                System.out.println(max);
+                for (int K = 0; K < array.length; K++) {
 
+                    int min = array[0];
+                    if (array[i] < min) {
+                        min = array[i];
+                        System.out.print(min +" ");
+
+                    }
+
+                }
             }
         }
-    }
-}
+    }}
+
+
 
 
 
