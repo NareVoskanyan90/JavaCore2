01f865ed3e4f1cb9de1627dfb1cedda0e9f31637
HP <nare.vos90@mail.ru>
2022-05-17 09:52:19 +0300

    charDemo2

--- src/Homevork/Arrayutil.java
+++ src/Homevork/Arrayutil.java
@@ -12,48 +12,41 @@ public class Arrayutil {
             if (array[i] > max) {
                 max = array[i];
                 System.out.println("max=" + max);
-
-                for (int j = 1; j < array.length; j++) {
-                    int min = 0;
-                    if (array[i] < min) {
-                        min = array[i];
-                        System.out.println("min=" + min);
-
-                        for (int յ = 0; յ < array.length; յ++) {
-                            if (array[i] % 2 == 0) {
-                                System.out.println(array[i]);
-                            }
-                        }
-                        for (int k = 0; k < array.length; k++) {
-                            int ii = 1;
-                            if (array[i] % 2 != 0) {
-                            }
-                        }
-                        int count = 1;
-                        for (int m = 0; m < array.length; m++) {
-                            if (array[i] % 2 == 0) {
-                                System.out.println(array[i]);
-                                count++;
-                                System.out.println("count" + count);
-                            }
-                        }
-                        int evencount = 0;
-                        for (int h = 0; h < array.length; h++) {
-                            if (array[i] % 2 != 0) {
-                                evencount++;
-                            }
-                            System.out.println("evencount" + evencount);
-                        }
-                    }
-                    int sum = 0;
-                    for (int s = 0; s < array.length; s++) {
-                        sum = +array[i];
-                        System.out.println("sum" + sum);
-                    }
-
-                    System.out.println("sum" + sum / array.length);
-                }
             }
         }
+
+        for (int j = 1; j < array.length; j++) {
+            int min = 0;
+            if (array[i] < min) {
+                min = array[i];
+                System.out.println("min=" + min);
+            }
+
+        }
+        int count = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 == 0) {
+                count++;
+            }
+        }
+        int evenCount = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 != 0) {
+                evenCount++;
+            }
+        }
+        System.out.println("evecount" + evenCount);
     }
 }
+
+                            int sum = 0;
+                for(int i=0;i<array.length;i++){
+                 sum+=array[i];
+        }
+    }
+        System.out.println("sum"+sum);
+
+        }}
+        }
+
+
