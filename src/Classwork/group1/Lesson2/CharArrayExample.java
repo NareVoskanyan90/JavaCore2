a93a97707ebadc98d7f440c08344c6aaaaf6a740
HP <nare.vos90@mail.ru>
2022-05-20 11:41:43 +0300

    charDemo2

---
+++ src/Classwork/group1/Lesson2/CharArrayExample.java
@@ -0,0 +1,15 @@
+package Classwork.group1.Lesson2;
+
+public class CharArrayExample {
+
+    public static void main(String[] args) {
+
+        char[] chars = {'j', 'a', 'v', 'a', 'l', 'o', 'v', 'e',};
+        char c='o';
+        for (int i = 1; i >'o'; i++) {
+            System.out.print("char=" + chars);
+
+
+        }
+    }
+}
